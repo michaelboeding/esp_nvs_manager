13ff31f466ee51d0f69aceb3361978557a9e577d
Michael Boeding <mjbn46@umsystem.edu>
2023-06-20 20:16:45 -0500

    Final changes needs for the nvs to work

--- NVSManager.h
+++ NVSManager.h
@@ -6,8 +6,13 @@
 #include "nvs.h"
 #include <string>
 #include "I_Serializable.h"
+#include <vector>
+#include <stdexcept>
 
 class NVSManager {
+
+private:
+    nvs_handle_t nvsHandle;
 public:
     //constructor and destructor
     NVSManager(const std::string namespaceName) {
@@ -23,33 +28,40 @@ public:
     }
 
     //template functions - note these have to be in the header file not the cpp file due to the way templates work
-
     esp_err_t save(const std::string& key, I_Serializable& object) {
+        esp_err_t ret;
         // Serialize the object to a string
         std::string serialized = object.serialize();
         // Save the serialized object to NVS
-        return nvs_set_blob(nvsHandle, key.c_str(), serialized.data(), serialized.size());
+        nvs_set_blob(nvsHandle, key.c_str(), serialized.data(), serialized.size());
+        ret = nvs_commit(nvsHandle);
+        return ret;
     }
 
     esp_err_t load(const std::string& key, I_Serializable& object) {
-         // Get the size of the saved data
+        // Get the size of the saved data
         size_t required_size;
         esp_err_t err = nvs_get_blob(nvsHandle, key.c_str(), NULL, &required_size);
-        if (err != ESP_OK) return err;
+        if (err != ESP_OK) {
+            printf("Failed to get the size of saved data. Error code: %d\n", err);
+            return err;
+        }
 
         // Load the saved data
         std::vector<uint8_t> buffer(required_size);
         err = nvs_get_blob(nvsHandle, key.c_str(), buffer.data(), &required_size);
-        if (err != ESP_OK) return err;
-
+        if (err != ESP_OK) {
+            printf("Failed to load the saved data. Error code: %d\n", err);
+            return err;
+        }
         // Deserialize the data to the object
         std::string serialized(buffer.begin(), buffer.end());
         object.deserialize(serialized);
         return ESP_OK;
     }
 
-private:
-    nvs_handle_t nvsHandle;
+
+
 };
 
 
